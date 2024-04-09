a209d0a2e5679143e727cbea213da252fc2e214b
jhShin1557 <sinmagic1@naver.com>
2024-04-10 00:09:00 +0900

    프로젝트 생성

---
+++ mvc03/src/main/java/com/example/mvc03/Mvc03Application.java
@@ -0,0 +1,13 @@
+package com.example.mvc03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mvc03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mvc03Application.class, args);
+	}
+
+}
