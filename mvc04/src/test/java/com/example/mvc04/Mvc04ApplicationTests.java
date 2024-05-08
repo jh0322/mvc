88405e4ba567268236a2276cc2144b1744b3da53
jhShin1557 <sinmagic1@naver.com>
2024-05-08 23:23:28 +0900

    mvc04 프로젝트 생성

---
+++ mvc04/src/test/java/com/example/mvc04/Mvc04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvc04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mvc04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
